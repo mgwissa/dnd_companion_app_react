a186b819fbe76e3dff1ad2b03e825074861b9db9
Mike Wissa <wissam@detroittrading.com>
2023-05-19 10:02:40 -0400

    bookmark

--- dnd-companion-app-react/src/components/header/new-header.js
+++ dnd-companion-app-react/src/components/header/new-header.js
@@ -23,16 +23,17 @@ const NewHeader = () => {
           </div>
           <div className="the-pages">
             {Object.keys(pages).forEach((key) => {
+              console.log("key", key);
               <a
-                key={pages[key].name}
-                href={pages[key].path}
+                key={key}
+                href={key}
                 className={
                   pages[key].current
                     ? "bg-gray-900 text-white"
                     : "text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
                 }
               >
-                {key.name}
+                {key}
               </a>;
             })}
           </div>
